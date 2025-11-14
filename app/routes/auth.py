e2a4b93d25cdc706f002ae2c51becadc739929bc
Akanksha C <akankshaprajwalbhat@gmail.com>
2025-11-14 13:08:08 +0530

    chore: updated signup logic

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
 from app.database import users_collection
 from app.core.security import hash_password, verify_password, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
 
 
 @router.post(
@@ -31,13 +39,17 @@ If the provided email already exists in the system, the request will fail with a
         },
     },
 )
-def signup(name: str, email: str, password: str):
-    if users_collection.find_one({"email": email}):
+def signup(user: User):
+    if users_collection.find_one({"email": user.email}):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists"
         )
     users_collection.insert_one(
-        {"name": name, "email": email, "password": hash_password(password)}
+        {
+            "name": user.name,
+            "email": user.email,
+            "password": hash_password(user.password),
+        }
     )
     return {"message": "User created successfully"}
 
